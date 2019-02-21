55ebab12f846817a94bc1a5e0882f933ba6a8298
Dee <dimitry.terekhov@gmail.com>
2019-02-22 00:52:33 +0300

    Remove wrong language mark after END_SRC add desktop-save (correctly saving theme with FONT on mac)

--- init.el
+++ init.el
@@ -25,4 +25,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:foreground "#bdc3ce" :background "#2a2f38")))))
